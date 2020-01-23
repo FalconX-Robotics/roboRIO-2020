42d9efe570a46c765f5a8d97e9086df211900f4c
lwbuchanan <luke1020will@gmail.com>
2020-01-22 16:34:38 -0800

    fixed speed issue

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -84,7 +84,7 @@ public class RobotContainer {
 	* @return the command to run in autonomous
 	*/
 	public Command getAutonomousCommand() {
-		// An ExampleCommand will run in autonomous
+		
 		return m_autoDrive;
 	}
 
