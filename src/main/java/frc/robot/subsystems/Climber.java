1d6c137aec0d28aa47a6e9f94a6972c6da28888a
Max Morehead <moreheadmax@gmail.com>
2020-01-27 22:36:41 -0800

    Add empty climber subsystem

---
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -0,0 +1,8 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Climber extends SubsystemBase {
+    // TODO: write out empty methods for climber subsystem (e.g. elevator up, elevator down, gondola left, gondola right)
+    // TODO: start implementing methods for climber subsystem
+}
